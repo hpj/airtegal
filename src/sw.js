c3d543e31675c0037e81ac36f2096e46188666cc
Kerolos Zaki <kerolos4zaki@gmail.com>
2019-07-18 02:04:01 +0200

    caching kbf apis woth network-first strategy

--- src/sw.js
+++ src/sw.js
@@ -57,3 +57,25 @@ workbox.routing.registerRoute(
     ]
   })
 );
+
+// Cache the geoip check with a cache-first strategy for 1 day
+workbox.routing.registerRoute(
+  new RegExp('https://geoip-db.com/jsonp'),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'kbf-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [ 0, 200 ]
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 1
+      })
+    ]
+  })
+);
+
+// Cache cards and packs with a network-first strategy
+workbox.routing.registerRoute(
+  new RegExp('https://kbf.herokuapp.com'),
+  new workbox.strategies.NetworkFirst()
+);
