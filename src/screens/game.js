4565ed50223a70f5ddc6a2deffeda85fed8e5104
Kerolos Zaki <kerolos4zaki@gmail.com>
2020-01-05 21:06:37 +0200

    moved from using a socket.io namespace to using a non-default path

--- src/screens/game.js
+++ src/screens/game.js
@@ -53,7 +53,7 @@ function connect()
   {
     try
     {
-      socket = io(API_ENDPOINT.replace('https', 'http') + '/io');
+      socket = io(API_ENDPOINT, { path: '/io' });
 
       socket.once('connect', resolve)
         .once('error', (e) =>
