d0c5ddaaac627b42254555d0f6a942e1e1f838cb
Kerolos Zaki <ker0olos@pm.me>
2021-09-01 11:52:17 +0200

    removed wasm from the configs

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -21,9 +21,6 @@ module.exports = {
       }
     ]
   },
-  experiments: {
-    syncWebAssembly: true
-  },
   plugins: [
     new DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
