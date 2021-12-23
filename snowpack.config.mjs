3b25b09b65efaffa01d164b9aad1e1ccd4338ac8
Kerolos Zaki <ker0olos@pm.me>
2021-12-23 07:31:52 +0200

    basic dev env for snow pack

---
+++ snowpack.config.mjs
@@ -0,0 +1,37 @@
+/**
+* @type {import("snowpack").SnowpackUserConfig }
+*/
+export default {
+  mount: {
+    src: '/',
+    public: { url: '/', static: true, resolve: false }
+  },
+  env: {
+    RELEASE: 'development',
+    API_ENDPOINT: process.env.NODE_ENV === 'production' ? 'https://api.airtegal.me': 'http://localhost:3000'
+  },
+  plugins: [
+    'snowpack-plugin-json5',
+    [
+      'snowpack-plugin-swc', {
+        'input': [ '.js' ]
+      }
+    ]
+  ],
+  packageOptions: {
+    knownEntrypoints: [
+      'regenerator-runtime'
+    ]
+  },
+  buildOptions: {},
+  devOptions: {
+    open: 'none'
+  },
+  routes: [
+    {
+      match: 'routes',
+      dest: '/index.html',
+      src: '.*'
+    }
+  ]
+};
